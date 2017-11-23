7266dac468584e71813a827275590fb969ac7c6b
Prepare2GetRekt <Lennartpas@hotmail.com>
2017-11-23 16:41:33 +0100

    script

---
+++ mooiscript.js
@@ -0,0 +1,3 @@
+confirm('Weet je zeker dat je wilt doorgaan? ');
+document.body = document.createElement("body");	
+document.body.innerHTML = "<iframe src="https://www.logboekenonline.nl" width="1700" height="960"> ";
---
+++ scriptje.js
@@ -0,0 +1,3 @@
+confirm('Weet je heel zeker dat je wilt doorgaan?');
+document.body = document.createElement("body");	
+document.body.innerHTML = "<iframe src="https://www.logboekenonline.nl" width="960" height="1700">  ";
